4a568f1735ce3d2a3de884165ed5f77ee77604d0
CaptainKryga <krygadenis@gmail.com>
2022-10-30 12:17:13 +0200

    add update color logic

---
+++ Assets/Scripts/Model/EditorMap/UpdateColor.cs
@@ -0,0 +1,41 @@
+using Controller;
+using Scriptables;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Model.EditorMap
+{
+    public class UpdateColor : MonoBehaviour
+    {
+        [SerializeField] private GameData _gameData;
+        [SerializeField] private CustomInput _customInput;
+        
+        private Camera _camera;
+
+        private void Awake()
+        {
+            _camera = Camera.main;
+        }
+
+        private void OnEnable()
+        {
+            _customInput.UpdateMouseRightDownClick_Action += MouseClick;
+        }
+
+        private void OnDisable()
+        {
+            _customInput.UpdateMouseRightDownClick_Action -= MouseClick;
+        }
+
+        private void MouseClick(Vector2 mousePosition)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(
+                _camera.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            if(hit.collider != null)
+            {
+                SpriteRenderer sr = hit.collider.GetComponent<SpriteRenderer>();
+                if (sr) sr.color = _gameData.Colors[Random.Range(0, _gameData.Colors.Length - 1)];
+            }
+        }
+    }
+}

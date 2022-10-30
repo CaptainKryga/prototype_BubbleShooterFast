82cd8f538a17b624f259096b00d17b7557cd5a3d
CaptainKryga <krygadenis@gmail.com>
2022-10-30 11:37:10 +0200

    add scriptable game setting's

---
+++ Assets/Scripts/Scriptables/GameSettings.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Scriptables
+{
+    [CreateAssetMenu(fileName = "GameSettings", menuName = "ScriptableObjects/GameSettings", order = 1)]
+    public class GameSettings : ScriptableObject
+    {
+        public bool IsCompany;
+        public int LevelId;
+    }
+}
